85686cbfe411e94b096ea08843354b150bbf7506
skysharkk <sky.light.blr@gmail.com>
2021-08-20 11:00:47 +0300

    fix: fix bug with path to ui file

--- view/window.py
+++ view/window.py
@@ -1,9 +1,13 @@
 from PyQt6 import uic, QtWidgets
+import os
+from PyQt6.QtGui import QIcon
 
-Form, _ = uic.loadUiType('../assets/uinterface.ui')
+Form, _ = uic.loadUiType(os.path.join(os.getcwd(), "assets", "uinterface.ui"))
 
 
 class Window(QtWidgets.QDialog, Form):
     def __init__(self) -> None:
         super().__init__()
+        self.setWindowIcon(
+            QIcon(os.path.join(os.getcwd(), "assets", "logo.png")))
         self.setupUi(self)
