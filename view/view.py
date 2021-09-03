a61d622a1fb40848a7a09c8987470278dade273f
skysharkk <sky.light.blr@gmail.com>
2021-09-03 14:32:20 +0300

    feat: implement init data functionality

--- view/view.py
+++ view/view.py
@@ -1,7 +1,7 @@
-from typing import ForwardRef
 from PyQt6.QtWidgets import QApplication, QWidget
 from uinterface import Ui_Form
 import sys
+from components import Input, GroupBox, Button, TreeWidget
 
 
 app = QApplication(sys.argv)
@@ -9,5 +9,25 @@ Form = QWidget()
 window = Ui_Form()
 window.setupUi(Form)
 Form.show()
+
+scale = Input(window.init_data_scale)
+thickness = Input(window.init_data_thickness)
+poly_type_group = GroupBox(window.init_data_type)
+add_type_btn = Button(window.init_data_add)
+delete_type_btn = Button(window.init_data_delete)
+types_table = TreeWidget(window.init_data_table)
+choose_pos_btn = Button(window.choose_autocad_el)
+
+
+def add_char() -> None:
+    poly_type = poly_type_group.get_checked_radio_button()
+    thickness_value = thickness.get_value()
+    types_table.add_element(int(thickness_value), poly_type)
+
+
+add_type_btn.connect_action(add_char)
+delete_type_btn.connect_action(types_table.delete_selected_widget_item)
+
+
 
 sys.exit(app.exec())
