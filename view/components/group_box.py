9f2fbd5ff6ef0fab5c00182099ac54039bab9df3
skysharkk <sky.light.blr@gmail.com>
2021-09-03 14:30:49 +0300

    feat: implement group box

---
+++ view/components/group_box.py
@@ -0,0 +1,13 @@
+from PyQt6.QtWidgets import QGroupBox, QRadioButton
+
+
+class GroupBox:
+    def __init__(self, group_box: QGroupBox) -> None:
+        self.group_box = group_box
+
+    def get_checked_radio_button(self) -> str:
+        for el in self.group_box.children():
+            if el.inherits("QRadioButton"):
+                radio_button: QRadioButton = el
+                if radio_button.isChecked():
+                    return radio_button.text()
