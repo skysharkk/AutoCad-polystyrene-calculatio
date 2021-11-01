5cf59f2f927bdb9436fc05f79ab23465d3f84e6d
skysharkk <sky.light.blr@gmail.com>
2021-11-01 21:15:17 +0300

    feat: add observer pattern ABC classes

---
+++ observer/observer.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+from .subject import Subject
+
+
+class Observer(ABC):
+    @abstractmethod
+    def update(self, subject: Subject) -> None:
+        pass
---
+++ observer/subject.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from .observer import Observer
+
+
+class Subject(ABC):
+    @abstractmethod
+    def attach(self, observer: Observer) -> None:
+        pass
+
+    @abstractmethod
+    def detach(self, observer: Observer) -> None:
+        pass
+
+    @abstractmethod
+    def notify(self) -> None:
+        pass
+
