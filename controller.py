6759645799c1cde93e24b911d68eec01a8bb043d
skysharkk <sky.light.blr@gmail.com>
2021-11-29 12:27:03 +0300

    fix: add QT_QPA_PLATFORM_PLUGIN_PATH to env

--- controller.py
+++ controller.py
@@ -1,5 +1,10 @@
 from view import View
 from model import Acad
+import sys
+import os
+
+
+sys.path.append(os.getcwd())
 
 
 def main():
@@ -19,10 +24,16 @@ def main():
         acad.draw_waste
     )
     ui.waste.calc_waste_btn.connect_action(
-        acad.draw_waste_result(ui.initial_data.scale, ui.results.table_data.calc_overall_volume)
+        acad.draw_waste_result(ui.initial_data.scale,
+                               ui.results.table_data.calc_overall_volume)
     )
     ui.window.exec_app()
 
 
 if __name__ == "__main__":
+    import PyQt5
+
+    dirname = os.path.dirname(PyQt5.__file__)
+    plugin_path = os.path.join(dirname, 'Qt5', 'plugins', 'platforms')
+    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
     main()
